dc01431afb3acdf0281f785cb0102da4f24076bf
bryonbsmb1 <87393401+bryonbsmb1@users.noreply.github.com>
2021-10-04 21:20:22 -0400

    setup css and started on the JavaScript file

---
+++ script.js
@@ -0,0 +1,50 @@
+// setting up my HTML for jscript
+
+var quizBody = document.getElementById("quiz");
+var resultsEl = document.getElementById("result");
+var finalScoreEl = document.getElementById("finalScore");
+var gameoverDiv = document.getElementById("gameover");
+var questionsEl = document.getElementById("questions");
+var quizTimer = document.getElementById("timer");
+var startQuizButton = document.getElementById("startbtn");
+var startQuizDiv = document.getElementById("startpage");
+var highscoreContainer = document.getElementById("highscoreContainer");
+var highscoreDiv = document.getElementById("high-scorePage");
+var highscoreInputName = document.getElementById("initials");
+var highscoreDisplayName = document.getElementById("highscore-initials");
+var endGameBtns = document.getElementById("endGameBtns");
+var submitScoreBtn = document.getElementById("submitScore");
+var highscoreDisplayScore = document.getElementById("highscore-score");
+var buttonA = document.getElementById("a");
+var buttonB = document.getElementById("b");
+var buttonC = document.getElementById("c");
+var buttonD = document.getElementById("d");
+
+// quiz questions
+var quizQuestions = [
+    {
+    question: "Which answer is good definition of JavaScript?",
+    choiceA: "A language that allows you to implement complex features on web pages",
+    choiceB: "A system for tagging text files to achieve font, color, graphic, and hyperlink effects on Web pages.",
+    choiceC: "A rule-based language — you define rules specifying groups of styles that should be applied to particular elements or groups of elements",
+    choiceD: "A language that came from Jedi's",
+    correctAnswer: "a"},
+    {
+        question: "In JavaScript variables are containers that store values, you start delaring a varible with what?",
+        choiceA: "let",
+        choiceB: "if",
+        choiceC: "var",
+        choiceD: "both A and C",
+        correctAnswer: "D"},
+
+
+
+
+    
+
+
+
+
+
+}]
+    
